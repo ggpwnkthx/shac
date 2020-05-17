1a1727c592d6c25ab6b68ef63f61ceee463770b1
ggpwnkthx <ibjessup@gmail.com>
2020-05-17 15:47:08 -0400

    network config via container

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,43 +1,67 @@
 #!/bin/sh
 
 # Default variables
+data_dir=${data_dir:="/srv/cluster"}
 orch_vlan_link=${orch_vlan_link:="eth0"}
 orch_vlan_id=${orch_vlan_id:=2}
 orch_vlan_name=${orch_vlan_name:="orchestration"}
 cidr=${cidr:="10.2.0.0/20"}
-cluster_user=${cluster_user:="cluster-manager"}
-cluster_password=${cluster_password:="$(date +%s | sha256sum | base64 | head -c 32 ; echo)"}
-data_dir=${data_dir:="/srv/cluster"}
-seaweedfs=${seaweedfs:="$data_dir/seaweedfs/mount"}
-datacenter=${datacenter:="1"}
-rack=${rack:="1"}
 
 # Dynamic Variables
 BASEPATH=$( cd ${0%/*} && pwd -P )
 
+# Build all the container images located in the containers/local directory reletive to this script
 build_container_images() {
     if [ "$(docker image inspect local/base 2>/dev/null)" = "[]" ]; then 
         cd $BASEPATH/containers/base
-        sudo docker build . -t local/base
+        docker build . -t local/base
     fi
     for img in $(ls -1 $BASEPATH/containers/local); do
         if [ -d $BASEPATH/containers/local/$img ]; then
             if [ "$(docker image inspect local/$img 2>/dev/null)" = "[]" ]; then 
                 cd $BASEPATH/containers/local/$img
-                sudo docker build . -t local/$img
+                docker build . -t local/$img
             fi
         fi
     done
 }
 
-link_toolkit() {
-    sudo chmod +x $BASEPATH/scripts/toolkit.sh
-    sudo ln -s $BASEPATH/scripts/toolkit.sh /usr/bin/toolkit
+# Restart docker daemon in the most convenient way available
+restart_docker() {
+    if [ -f /etc/init.d/docker ]; then /etc/init.d/docker restart; return; fi
+    if [ ! -z "$(which service)" ]; then service docker restart; return; fi
+    if [ ! -z "$(which systemctl)" ]; then systemctl restart docker.service; return; fi
+}
+
+# Use the network-manager image to configure the host's network interfaces
+network_config() {
+    docker run --rm \
+        --cap-add NET_ADMIN \
+        --net=host \
+        -v /srv/cluster/local:/mnt/local
+        local/network-manager \
+        setup-orch-net.sh $orch_vlan_link $orch_vlan_name $orch_vlan_id $cidr
+}
+
+fix_docker_bridge() {
+    cat /etc/docker/daemon.json | sudo toolkit jq '."bip"="test"' > /etc/docker/daemon.json
+    restart_docker
 }
 
 bootstrap() {
+    mkdir -p $data_dir/local
     build_container_images
-    link_toolkit
+    network_config
 }
 
-bootstrap
+wrapper() {
+    bootstrap
+}
+
+# Rerun self if not root
+user="$(id -un 2>/dev/null || true)"
+if [ "$user" != 'root' ]; then
+    sudo $0 $@
+else
+    wrapper
+fi
---
+++ containers/local/network-manager/setup-orch-net.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+VLAN_LINK=$1
+VLAN_NAME=$2
+VLAN_ID=$3
+CIDR=$4
+
+if [ 24 -lt $(echo $CIDR | awk -F/ '{print $2}') ]; then
+    echo ""
+    echo "The provided bit mask of /$(echo $CIDR | awk -F/ '{print $2}') is too low for practical use."
+    echo "The CIDR provided will be divided into multiple subnets and VLANs."
+    echo "Any bit mask greater than 24 is not recommended, is unsupported, and will be rejected."
+    echo "A CIDR similar to 10.2.0.0/20 is recommended."
+    echo ""
+    exit 1
+fi
+BITMASK=$(($(echo $cidr | awk -F/ '{print $2}') + 2))
+
+# Add the vlan interface if it doesn't exist
+if [ -z "$(ip link show $VLAN_NAME)" ]; then
+    ip link add link $VLAN_LINK name $VLAN_NAME type vlan id $VLAN_ID
+fi
+
+# If no IP is detected, use dhclient to get one
+IP1=$(ip -j address | jq -r --arg i "$VLAN_NAME" '.[] | select(.ifname == $i) | .addr_info[] | select(.family == "inet") | .local')
+if [ -z "$IP1" ]; then
+    echo "timeout 10;" > /etc/dhcp/dhclient.conf
+    dhclient -1 $VLAN_NAME
+fi
+
+# If we didn't talk to a DHCP server, then we assumer we're the first on the network
+IP2=$(ip -j address | jq -r --arg i "$VLAN_NAME" '.[] | select(.ifname == $i) | .addr_info[] | select(.family == "inet") | .local')
+if [ -z "$IP2" ]; then
+    if [ -f /mtn/local/ip ]; then
+        IP3=$(cat /mtn/local/ip)
+    else
+        IP3=$(shift-ip $(ipcalc $CIDR | grep HostMin | awk '{print $2}') +1)/$BITMASK
+        # Save our IP address
+        echo $IP3 > /mtn/local/ip
+    fi
+    ip addr add $IP3 dev $VLAN_NAME
+fi
