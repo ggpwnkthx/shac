47f8ed733a716405f0bae52ee1bd33c59a3bfe43
ggpwnkthx <ibjessup@gmail.com>
2020-05-28 20:20:35 -0400

    seaweedfs fix

--- containers/swarm/seaweedfs/entrypoint.sh
+++ containers/swarm/seaweedfs/entrypoint.sh
@@ -8,13 +8,13 @@ NODE=$( \
     curl --unix-socket /var/run/docker.sock http://x/containers/$(hostname)/json 2>/dev/null | \
     jq -r '.Config.Labels."com.docker.swarm.node.id"'
 )
-if [ -z "$(echo $DATACENTER)" ]; then
+if [ -z "$DATACENTER" ]; then
     DATACENTER=$( \
         curl --unix-socket /var/run/docker.sock http://x/nodes/$NODE 2>/dev/null | \
         jq -r '.Spec.Labels.datacenter'
     )
 fi
-if [ -z "$(echo $RACK)" ]; then
+if [ -z "$RACK" ]; then
     RACK=$( \
         curl --unix-socket /var/run/docker.sock http://x/nodes/$NODE 2>/dev/null | \
         jq -r '.Spec.Labels.rack' \
@@ -42,23 +42,23 @@ get_local_filer() {
 case "$SERVICE" in
     'master')
         ARGS="$ARGS -port=80 -mdir=/data -volumePreallocate"
-        if [ ! -z "$(echo $MAX_VOLUME_SIZE)" ]; then ARGS="$ARGS -volumeSizeLimitMB=$MAX_VOLUME_SIZE"; fi
-        if [ ! -z "$(echo $REPLICATION)" ]; then ARGS="$ARGS -defaultReplication=$REPLICATION"; fi
+        if [ ! -z "$MAX_VOLUME_SIZE" ]; then ARGS="$ARGS -volumeSizeLimitMB=$MAX_VOLUME_SIZE"; fi
+        if [ ! -z "$REPLICATION" ]; then ARGS="$ARGS -defaultReplication=$REPLICATION"; fi
         if [ $(get_masters | wc -m) -gt 1 ]; then 
             ARGS="$ARGS -peers=$(get_masters)"
         fi
     ;;
     'volume')
         ARGS="$ARGS -port=80"
-        if [ ! -z "$(echo $DATACENTER)" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
-        if [ ! -z "$(echo $RACK)" ]; then ARGS="$ARGS -rack=$RACK"; fi
-        if [ ! -z "$(echo $MAX_VOLUMES)" ]; then ARGS="$ARGS -max=$MAX_VOLUMES"; fi
+        if [ ! -z "$DATACENTER" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
+        if [ ! -z "$RACK" ]; then ARGS="$ARGS -rack=$RACK"; fi
+        if [ ! -z "$MAX_VOLUMES" ]; then ARGS="$ARGS -max=$MAX_VOLUMES"; fi
         while [ $(get_masters | wc -m) -lt 1 ]; do sleep 1; done
         ARGS="$ARGS -dir=/data -mserver=$(get_masters)"
         ;;
     'filer')
         ARGS="$ARGS -port=80"
-        if [ ! -z "$(echo $DATACENTER)" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
+        if [ ! -z "$DATACENTER" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
         while [ $(get_masters | wc -m) -lt 1 ]; do sleep 1; done
         ARGS="$ARGS -master=$(get_masters)"
         ;;
@@ -72,7 +72,7 @@ case "$SERVICE" in
         if [ ! -f /run/secret/seaweedfs_key ]; then echo "Certificate key secret 'seaweedfs_key' not provided."; exit 1; fi
         if [ ! -f /run/secret/seaweedfs_cert ]; then echo "Certificate secret 'seaweedfs_cert' not provided."; exit 1; fi
         ARGS="$ARGS -key.file=/run/secret/key -cert.file=/run/secret/cert"
-        if [ ! -z "$(echo $DOMAIN_NAME)" ]; then ARGS="$ARGS --domainName=$DOMAIN_NAME"; fi
+        if [ ! -z "$DOMAIN_NAME" ]; then ARGS="$ARGS --domainName=$DOMAIN_NAME"; fi
         while [ $(get_local_filer | wc -w) -lt 1 ]; do sleep 1; done
         ARGS="$ARGS -filer=$(get_local_filer)"
         ;;
