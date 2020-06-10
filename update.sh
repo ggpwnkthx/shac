19891796cfcb43fc32033e46f250b694af1b5f93
ggpwnkthx <ibjessup@gmail.com>
2020-06-10 09:47:39 -0400

    updater

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Default variables
+BASEPATH=$( cd ${0%/*} && pwd -P )
+DATA_DIR=${DATA_DIR:="/srv/cluster"}
+
+get_running_updates() {
+    docker service ls | \
+    grep updater_updater | \
+    awk '{print $4}' | \
+    awk -F/ '{print $1}'
+}
+
+update_sequence() {
+    docker stack rm seaweedfs
+    env BASEPATH=$BASEPATH DATA_DIR=$DATA_DIR docker stack deploy -c $BASEPATH/containers/swarm/updater/docker-compose.yml updater
+    while [ $(get_running_updates) -gt 0 ]; do sleep 1; done
+    docker stack rm updater
+    $BASEPATH/start.sh
+}
+
+# Rerun self if not root
+user="$(id -un 2>/dev/null || true)"
+if [ "$user" != 'root' ]; then
+    sudo $0 $@
+else
+    update_sequence
+fi
