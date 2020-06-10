83b769d85103d84ec5ba9dec1eaad8abd3f08287
ggpwnkthx <ibjessup@gmail.com>
2020-06-10 11:07:40 -0400

    update stacks

--- update.sh
+++ update.sh
@@ -2,13 +2,20 @@
 
 # Default variables
 BASEPATH=$( cd ${0%/*} && pwd -P )
+DATA_DIR=${DATA_DIR:="/srv/cluster"}
+if [ -f $DATA_DIR/config ]; then
+    . $DATA_DIR/config
+fi
+
+update_stacks() {
+    env SEAWEEDFS_DIR=$DATA_DIR/seaweedfs docker stack deploy -c $BASEPATH/containers/swarm/seaweedfs/docker-compose.yml seaweedfs
+}
 
 update_sequence() {
-    docker stack rm seaweedfs
     env SRC=$BASEPATH docker stack deploy -c $BASEPATH/containers/swarm/updater/docker-compose.yml updater
     while [ "$(docker service ls | grep updater_updater | awk '{print $4}')" != "0/0" ]; do sleep 1; done
     docker stack rm updater
-    $BASEPATH/start.sh
+    update_stacks
 }
 
 # Rerun self if not root
