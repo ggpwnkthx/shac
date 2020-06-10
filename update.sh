be437cacb8f1648db25e2e9c47e03339f471fcd2
ggpwnkthx <ibjessup@gmail.com>
2020-06-10 10:37:23 -0400

    wait for updates to complete

--- update.sh
+++ update.sh
@@ -3,19 +3,12 @@
 # Default variables
 BASEPATH=$( cd ${0%/*} && pwd -P )
 
-get_running_updates() {
-    docker service ls | \
-    grep updater_updater | \
-    awk '{print $4}' | \
-    awk -F/ '{print $1}'
-}
-
 update_sequence() {
     docker stack rm seaweedfs
     env SRC=$BASEPATH docker stack deploy -c $BASEPATH/containers/swarm/updater/docker-compose.yml updater
-    #while [ $(get_running_updates) -gt 0 ]; do sleep 1; done
-    #docker stack rm updater
-    #$BASEPATH/start.sh
+    while [ "$(docker service ls | grep updater_updater | awk '{print $4}')" != "0/0" ]; do sleep 1; done
+    docker stack rm updater
+    $BASEPATH/start.sh
 }
 
 # Rerun self if not root
