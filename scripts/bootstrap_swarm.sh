96e5ded88edc9987d619e3ea84d42a50e27e83cf
ggpwnkthx <ibjessup@gmail.com>
2020-06-12 15:10:15 -0400

    ip fix

--- scripts/bootstrap_swarm.sh
+++ scripts/bootstrap_swarm.sh
@@ -55,8 +55,8 @@ init_docker_swarm() {
             -o com.docker.network.bridge.enable_ip_masquerade=true \
             docker_gwbridge
         docker swarm init \
-            --listen-addr $(echo $CIDR | awk -F/ '{print $1}'):2377 \
-            --advertise-addr $(echo $CIDR | awk -F/ '{print $1}'):2377 >/dev/null
+            --listen-addr $(echo $ORCH_VLAN_CIDR | awk -F/ '{print $1}'):2377 \
+            --advertise-addr $(echo $ORCH_VLAN_CIDR | awk -F/ '{print $1}'):2377 >/dev/null
     fi
 }
 
