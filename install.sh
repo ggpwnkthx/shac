e86bf6175fa0b614da5a6a94bd41ebd383eae5c9
ggpwnkthx <ibjessup@gmail.com>
2020-06-12 14:36:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@
 BASEPATH=$( cd ${0%/*} && pwd -P )
 
 install() {
+    chmod +x $BASEPATH/start.sh
     case "$(cat /proc/1/comm)" in
         systemd)
-            sed -i "s/^ExecStart.*/ExecStart=$(echo $BASEPATH | sed 's/\//\\\//g')\/start.sh/" $BASEPATH/services/systemd
-            if [ ! -f /etc/systemd/system/shac ]; then
-                ln -s $BASEPATH/services/systemd /etc/systemd/system/shac.service
-            fi
+            if [ -f /etc/systemd/system/shac.service ]; then rm /etc/systemd/system/shac.service; fi
+            sed "s/^ExecStart.*/ExecStart=$(echo $BASEPATH | sed 's/\//\\\//g')\/start.sh/" $BASEPATH/services/systemd > /etc/systemd/system/shac.service
             systemctl daemon-reload
             systemctl enable shac.service
             systemctl start shac.service
