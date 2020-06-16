d1c901623b03a4441f268da03c015d3fad2ce436
ggpwnkthx <ibjessup@gmail.com>
2020-06-16 11:11:45 -0400

    test

--- scripts/bootstrap_swarm.sh
+++ scripts/bootstrap_swarm.sh
@@ -104,8 +104,8 @@ join_docker_swarm() {
 bootstrap() {
     # Use service discovery to bootstrap or join the cluster
     service_discovery
-    swarm_id=$(curl --unix-socket /var/run/docker.sock http://x/info 2>/dev/null | jq -r '.Swarm.NodeID')
-    while [ $? > 0 ]; do swarm_id=$(curl --unix-socket /var/run/docker.sock http://x/info 2>/dev/null | jq -r '.Swarm.NodeID'); done
+    curl --unix-socket /var/run/docker.sock http://x/info
+    while [ $? > 0 ]; do curl --unix-socket /var/run/docker.sock http://x/info; done
     if [ -z "$swarm_id" ]; then
         if [ -z "$DOCKER_SWARM_MANAGER_JOIN" ]; then
             init_docker_swarm
