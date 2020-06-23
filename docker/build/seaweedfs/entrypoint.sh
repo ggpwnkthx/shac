21f8b11c0975260ca1a93e54be6e49408d6326e9
ggpwnkthx <ibjessup@gmail.com>
2020-06-23 10:31:23 -0400

    refactor for swarm

--- docker/build/seaweedfs/entrypoint.sh
+++ docker/build/seaweedfs/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Discover service mode by environmental variable or through swarm label
 if [ -z "$SERVICE" ]; then
@@ -8,26 +8,35 @@ if [ -z "$SERVICE" ]; then
         awk -F_ '{print $NF}'
     )
 fi
-
 # Discover container ID
 ID=$( \
     curl --unix-socket /var/run/docker.sock http://x/containers/$(hostname)/json 2>/dev/null | \
-    jq -r '.Id'
+    jq -r '.Id' \
 )
 # Discover the local node ID
 NODE_ID=$( \
     curl --unix-socket /var/run/docker.sock http://x/containers/$(hostname)/json 2>/dev/null | \
-    jq -r '.Config.Labels."com.docker.swarm.node.id"'
+    jq -r '.Config.Labels."com.docker.swarm.node.id"' \
 )
-
 # Get local docker node hostname by environmental variable or through swarm label
 if [ -z "$NODE" ]; then
     NODE=$( \
         curl --unix-socket /var/run/docker.sock http://x/nodes/$NODE_ID | \
         jq -r '.Description.Hostname' | \
-        awk -F. '{print $1}'
+        awk -F. '{print $1}' \
     )
 fi
+# Discover Task ID
+TASK_ID=$( \
+    curl --unix-socket /var/run/docker.sock http://x/containers/$(hostname)/json 2>/dev/null | \
+    jq -r '.Config.Labels."com.docker.swarm.task.id"' \
+)
+# Discover IP address
+IP=$(
+    curl --unix-socket /var/run/docker.sock http://x/networks/seaweedfs_default 2>/dev/null | \
+    jq --arg ID $ID -r '.Containers."\($ID)".IPv4Address' | \
+    awk -F/ '{print $1}'
+)
 
 # Discover datacenter and rack details via environmental variables or through node labels.
 # If nothing found, unset the variables so seaweedfs uses internal defaults.
@@ -46,43 +55,17 @@ if [ -z "$RACK" ]; then
     if [ "$RACK" = "null" ]; then unset RACK; fi
 fi
 
-# Discover all the container IDs for a given service name
-get_all_service_ids() {
-    ids=$( \
-        curl --unix-socket /var/run/docker.sock http://x/containers/json 2>/dev/null | \
-        jq -r --arg SERVICE $1 '.[] | select(.State!="exited") | select(.Labels."com.docker.swarm.service.name"==$SERVICE) | .Id'
-    )
-    for id in $ids; do
-        if [ "$id" != "$ID" ]; then echo $id; fi
-    done
-}
-get_local_service_ids() {
-    curl --unix-socket /var/run/docker.sock http://x/containers/json 2>/dev/null | \
-    jq -r --arg NODE_ID $NODE_ID --arg SERVICE $1 '.[] | select(.State!="exited") | select(.Labels."com.docker.swarm.node.id"==$NODE_ID) | select(.Labels."com.docker.swarm.service.name"==$SERVICE) | .Id'
-}
-# Get the health status of a container by it's ID
-get_container_status() {
-    curl --unix-socket /var/run/docker.sock http://x/containers/$1/json 2>/dev/null | jq -r '.State.Health.Status'
-}
-# Wait until all given container IDs are in a healthy state
-wait_for_containers() {
-    for id in $@; do
-        echo "Waiting for $id..." 
-        while [ "healthy" != "$(get_container_status $id)" ]; do sleep 1; done
-    done
-}
-wait_for_masters() {
-    while [ -z "$(get_all_service_ids seaweedfs_master)" ]; do sleep 1; done
-}
-
-get_container_ip() {
-    curl --unix-socket /var/run/docker.sock http://x/networks/seaweedfs_default 2>/dev/null | \
-    jq --arg ID $1 -r '.Containers."\($ID)".IPv4Address' | \
-    awk -F/ '{print $1}'
+# String Formatting Functions
+formatConnectionString() {
+    for ip in $@; do
+        echo $ip:80
+    done | \
+    xargs | \
+    sed -e 's/\s\+/,/g'
 }
 
 # Wait for the filer's store to be healthy
-wait_for_store() {
+waitForFilerStore() {
     store=$(head -n 1 /etc/seaweedfs/filer.toml | sed 's/[][]//g')
     echo "Waiting for $store..." 
     case $store in
@@ -121,53 +104,106 @@ wait_for_store() {
     esac
 }
 
-get_masters() {
-    for id in $(get_all_service_ids seaweedfs_master); do
-        echo "$(get_container_ip $id):80"
-    done | \
-    xargs | \
-    sed -e 's/\s\+/,/g'
+# Swarm functions
+getAllTaskIDsByServiceName() {
+    ids=$( \
+        curl --unix-socket /var/run/docker.sock http://x/tasks 2>/dev/null | \
+        jq -r --arg SERVICE $1 '
+            .[] |
+            select(.Spec.ContainerSpec.Labels."com.docker.stack.namespace"=="seaweedfs") |
+            select(.DesiredState=="running") |
+            select(.Spec.Networks[].Aliases[] | contains($SERVICE)) |
+            .ID
+        '
+    )
+    for id in $ids; do
+        if [ "$id" != "$TASK_ID" ]; then echo $id; fi
+    done
+}
+getLocalTaskIDsByServiceName() {
+    curl --unix-socket /var/run/docker.sock http://x/tasks 2>/dev/null | \
+    jq -r --arg NODE_ID $NODE_ID --arg SERVICE $1'
+        .[] |
+        select(.Node==$NODE_ID) |
+        select(.Spec.ContainerSpec.Labels."com.docker.stack.namespace"=="seaweedfs") |
+        select(.DesiredState=="running") |
+        select(.Spec.Networks[].Aliases[] | contains($SERVICE)) |
+        .ID
+    '
+}
+getTaskIPByIDs() {
+    for id in $@; do
+        curl --unix-socket /var/run/docker.sock http://x/tasks/$id 2>/dev/null | \
+        jq -r '.NetworksAttachments[] | select(.Network.Spec.Name=="seaweedfs_default") | .Addresses[]' | \
+        awk -F/ '{print $1}'
+    done
+}
+getConnectionStringByServiceName() {
+    formatConnectionString $(getTaskIPByIDs $(getAllTaskIDsByServiceName $1))
+}
+getLocalConnectionStringByServiceName() {
+    formatConnectionString $(getTaskIPByIDs $(getLocalTaskIDsByServiceName $1))
 }
 
+waitForHTTP() {
+    echo "Waiting for $1 ..."
+    while [ -z "$html" ]; do
+        html=$(curl -f $1 2>/dev/null)
+    done
+}
+waitForServicesByConnectionString() {
+    IFS=',' read -r -a nodes <<< "$1"
+    for node in ${nodes[@]}; do
+        waitForHTTP http://$node$2
+    done
+}
+
+# Configure arguments
 case "$SERVICE" in
     'master')
-        ARGS="$ARGS -ip=$(get_container_ip $ID) -port=80 -mdir=/data -volumePreallocate"
+        ARGS="$ARGS -ip=$IP -port=80 -mdir=/data -volumePreallocate"
         if [ ! -z "$MAX_VOLUME_SIZE" ]; then ARGS="$ARGS -volumeSizeLimitMB=$MAX_VOLUME_SIZE"; fi
         if [ ! -z "$REPLICATION" ]; then ARGS="$ARGS -defaultReplication=$REPLICATION"; fi
-        if [ $(get_masters | wc -w) -gt 0 ]; then 
-            wait_for_containers $(get_all_service_ids seaweedfs_master)
-            ARGS="$ARGS -peers=$(get_masters)"
+        peers=$(getConnectionStringByServiceName master)
+        if [ -z "$peers" ]; then 
+            ARGS="$ARGS -peers=$peers"
+            waitForServicesByConnectionString $peers
         fi
     ;;
     'volume')
+        ARGS="$ARGS -ip=$IP -port=80 -dir=/data"
         if [ ! -z "$DATACENTER" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
         if [ ! -z "$RACK" ]; then ARGS="$ARGS -rack=$RACK"; fi
         if [ ! -z "$MAX_VOLUMES" ]; then ARGS="$ARGS -max=$MAX_VOLUMES"; fi
-        wait_for_masters
-        wait_for_containers $(get_all_service_ids seaweedfs_master)
-        ARGS="$ARGS -ip=$(get_container_ip $ID) -port=80 -dir=/data -mserver=$(get_masters)"
+        peers=$(getConnectionStringByServiceName master)
+        if [ -z "$peers" ]; then 
+        ARGS="$ARGS -mserver=$peers"
+            waitForServicesByConnectionString $peers
+        fi
     ;;
     'filer')
+        ARGS="$ARGS -ip=$IP -port=80"
         if [ ! -z "$DATACENTER" ]; then ARGS="$ARGS -dataCenter=$DATACENTER"; fi
-        wait_for_masters
-        wait_for_containers $(get_all_service_ids seaweedfs_master)
-        wait_for_store
-        ARGS="$ARGS -ip=$(get_container_ip $ID) -port=80 -master=$(get_masters)"
-    ;;
-    'mount')
-        wait_for_containers $(get_local_service_ids seaweedfs_filer)
-        ARGS="$ARGS -dir=/data -filer=$(get_container_ip $(get_local_service_ids seaweedfs_filer)):80"
+        if [ ! -z "$TASK_ID" ]; then
+        peers=$(getConnectionStringByServiceName master)
+        if [ -z "$peers" ]; then 
+            ARGS="$ARGS -master=$peers"
+            waitForServicesByConnectionString $peers
+        fi
+        waitForFilerStore
     ;;
     's3')
         if [ -f /run/secret/seaweedfs_key ]; then ARGS="$ARGS -key.file=/run/secret/seaweedfs_key"; fi
         if [ -f /run/secret/seaweedfs_cert ]; then ARGS="$ARGS -cert.file=/run/secret/seaweedfs_cert"; fi
         if [ ! -z "$DOMAIN_NAME" ]; then ARGS="$ARGS --domainName=$DOMAIN_NAME"; fi
-        wait_for_containers $(get_local_service_ids seaweedfs_filer)
-        ARGS="$ARGS -port=80 --filer=$(get_container_ip $(get_local_service_ids seaweedfs_filer)):80"
+        peers=$(getLocalConnectionStringByServiceName filer)
+        waitForServicesByConnectionString $peers
+        ARGS="$ARGS -port=80 --filer=$peers"
     ;;
     'webdav')
-        wait_for_containers $(get_local_service_ids seaweedfs_filer)
-        ARGS="$ARGS -port=80 -filer=$(get_container_ip $(get_local_service_ids seaweedfs_filer)):80"
+        peers=$(getLocalConnectionStringByServiceName filer)
+        waitForServicesByConnectionString $peers
+        ARGS="$ARGS -port=80 -filer=$peers"
     ;;
 esac
 echo "Running: /usr/bin/weed $SERVICE $ARGS"
